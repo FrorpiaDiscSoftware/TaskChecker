c44f1762eaebdcdcb7bdb8c407d0b8d8798a8114
FrorpiaDiscSoftware <nonamelv2@gmail.com>
2022-01-22 10:41:03 +0900

    ・UserControlBaseをとりあえず作ってみた。 　⇒ただ、通常のUserControlの規定クラスとするのは無理そう。

---
+++ TaskChecker/TaskChecker/GuiControl/_Base/IUserControlBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TaskChecker.GuiControl
+{
+    public interface IUserControlBase
+    {
+        int         id          { get; }//このコントロールのID
+        UserControl thisControl { get; }//本クラスの基本コントロールインスタンス
+        //-----------------------------------------------------------------------------
+    }
+}
---
+++ TaskChecker/TaskChecker/GuiControl/_Base/UserControlBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace TaskChecker.GuiControl
+{
+    public abstract class UserControlBase<EntityT> : UserControl , IUserControlBase
+        where EntityT : UserControlBase<EntityT>.EntityBase
+    {
+        protected int _id = 0;//このコントロールのID
+        //-----------------------------------------------------------------------------
+        [Browsable(true),Description("このコントロールのID")]
+        public int         id          { get => _id;  }
+        public UserControl thisControl { get => this; }//本クラスの基本コントロールインスタンス
+        //-----------------------------------------------------------------------------
+        
+        
+        //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
+        //↓追加定義クラス関連
+        
+        /// <summary>
+        /// 表示内容の一括設定データ基本クラス
+        /// </summary>
+        public class EntityBase
+        {
+            /// <summary>
+            /// このコントロールのID(必要に応じて設定)
+            /// </summary>
+            public int id = 0;
+        }
+		
+		
+        //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
+        //↓基本機能関連
+		
+        /// <summary>
+        /// 表示内容の一括セットアップを行なう関数
+        /// </summary>
+        /// <param name="pEntity">全表示内容の設定</param>
+        public void Setup( EntityT pEntity )
+        {
+            if ( pEntity == null ) { return; }
+
+            _id = pEntity.id;
+            
+            OnSetup(pEntity);
+        }
+		
+		
+        //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
+        //↓派生先イベント関連
+		
+        /// <summary>
+        /// 表示内容の一括セットアップ時イベント関数(派生先呼び出し用)
+        /// </summary>
+        /// <param name="pEntity">全表示内容の設定</param>
+        protected abstract void OnSetup( EntityT pEntity );
+    }
+}
