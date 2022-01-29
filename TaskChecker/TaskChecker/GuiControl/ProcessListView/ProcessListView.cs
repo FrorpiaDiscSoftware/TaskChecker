c3e69cc74b18bd99d1ebb1cba922f4a7d27f0de0
FrorpiaDiscSoftware <nonamelv2@gmail.com>
2022-01-30 01:56:46 +0900

    ・UserControlの基本クラス作成用に一旦UP。

--- TaskChecker/TaskChecker/GuiControl/ProcessListView/ProcessListView.cs
+++ TaskChecker/TaskChecker/GuiControl/ProcessListView/ProcessListView.cs
@@ -1,9 +1,19 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace TaskChecker.GuiControl
 {
     public partial class ProcessListView : UserControl
     {
+        private const int TITLE_HEIGHT = 25;//作業工程タイトルの高さ
+        //-----------------------------------------------------------------------------
+        private int                                   _id       = 0;                                          //このコントロールのID(Indexに使用)
+        private List<ListItemContainer<TaskListItem>> _children = new List<ListItemContainer<TaskListItem>>();//子の作業工程リスト
+        //-----------------------------------------------------------------------------
+        public int                          id              { get => _id; }//このコントロールのID(Indexに使用)
+        public Action<ProcessListView,Size> onResizeRequest { get; set; } = null;//リサイズ発生とリクエストイベント※親はこのイベントに応じて自身のサイズを変える必要あり。
         //-----------------------------------------------------------------------------
 
 
@@ -13,7 +23,19 @@ namespace TaskChecker.GuiControl
         /// <summary>
         /// 表示内容の一括設定データクラス
         /// </summary>
-        public class Entity { }
+        public class Entity
+        {
+            /// <summary>
+            /// このコントロールのID(Indexに使用)
+            /// </summary>
+            public int id = 0;
+            
+            /// <summary>
+            /// リサイズ発生とリクエストイベント<br />
+            /// ※親はこのイベントに応じて自身のサイズを変える必要あり。
+            /// </summary>
+            public Action<ProcessListView,Size> onResizeRequest = null;
+        }
 
 
         //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
@@ -48,6 +70,24 @@ namespace TaskChecker.GuiControl
         public void Setup( Entity pEntity )
         {
             if ( pEntity == null ) { return; }
+            
+            onResizeRequest = pEntity.onResizeRequest;
+            
+            _id = pEntity.id;
+        }
+		
+        /// <summary>
+        /// このコントロールのリサイズを行なう関数<br />
+        /// ※Dock状態に応じて対応方法を調整します。<br />
+        /// ※自身でサイズ変更を行なう場合は本関数を必ず使用してください。
+        /// </summary>
+        /// <param name="pSize">新しいサイズ</param>
+        private void ReSize( Size pSize )
+        {
+            if ( pSize == Size ) { return; }
+            if ( pSize.Height < TITLE_HEIGHT ) { pSize.Height = TITLE_HEIGHT; }
+            if ( Dock != DockStyle.Fill ) { Size = pSize; } else { onResizeRequest.Invoke(this, pSize); }
+            Update();
         }
     }
 }
