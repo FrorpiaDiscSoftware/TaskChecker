f99fac7558f15ba6bea30203fa7fe778b19d8fb9
FrorpiaDiscSoftware <nonamelv2@gmail.com>
2022-01-29 23:29:32 +0900

    ・ProcessListViewのプレーンな状態を一旦UP。

---
+++ TaskChecker/TaskChecker/GuiControl/ProcessListView/ProcessListView.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows.Forms;
+
+namespace TaskChecker.GuiControl
+{
+    public partial class ProcessListView : UserControl
+    {
+        //-----------------------------------------------------------------------------
+
+
+        //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
+        //↓追加定義クラス関連
+
+        /// <summary>
+        /// 表示内容の一括設定データクラス
+        /// </summary>
+        public class Entity { }
+
+
+        //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
+        //↓システム関連
+
+        /// <summary>
+        /// コンストラクタ(引数なし)
+        /// </summary>
+        public ProcessListView()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="pEntity">初期設定</param>
+        public ProcessListView( Entity pEntity )
+        {
+            InitializeComponent();
+            Setup(pEntity);
+        }
+
+
+        //～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～～
+        //↓基本機能関連
+
+        /// <summary>
+        /// 表示内容の一括セットアップを行なう関数
+        /// </summary>
+        /// <param name="pEntity">全表示内容の設定</param>
+        public void Setup( Entity pEntity )
+        {
+            if ( pEntity == null ) { return; }
+        }
+    }
+}
